d19498bdba63aa0667c9d21136e8953f53219abc
Anderson B. Marques <andbmarques@gmail.com>
2026-03-01 14:49:40 -0300

    Add Styles to Pages

--- netmanager/forms.py
+++ netmanager/forms.py
@@ -12,6 +12,11 @@ class AssetForm(forms.ModelForm):
     class Meta:
         model = Asset
         fields = ['hostname', 'address', 'type', 'customer']
+        widgets = {
+            'asset_type': forms.Select(attrs={
+                'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-pointer',
+            }),
+        }
 
 class AssetTypeForm(forms.ModelForm):
     class Meta:
